147bf72339c3e97649b261777bef12dca280466f
Alex Rhea <alex.rhea@gmail.com>
2014-02-17 16:12:12 -0500

    Added colors to command line output and better error reporting.

--- favbot.js
+++ favbot.js
@@ -1,16 +1,18 @@
 (function() {
-  var delay, favoriteTweet, log, moment, settings, startTracking, tweeper, twitter;
+  var colors, delay, favoriteTweet, log, moment, settings, startTracking, tweeper, twitter;
 
   twitter = require('twit');
 
   moment = require('moment');
+
+  colors = require('colors');
 
   settings = require('./settings/settings.json');
 
   tweeper = new twitter(settings.twitter);
 
   log = function(str) {
-    return console.log("[favbot] " + str);
+    return console.log('[' + 'favbot'.green + ']', str);
   };
 
   delay = function(ms, func) {
@@ -19,13 +21,15 @@
 
   favoriteTweet = function(tweet) {
     return tweeper.post('favorites/create', {
-      id: "" + tweet.id_str
-    }, function(errors, tweet) {
-      if (errors) {
-        log(errors);
-        return;
+      id: tweet.id
+    }, function(error, tweet) {
+      var twitterError;
+      if (error) {
+        twitterError = JSON.parse(error.twitterReply);
+        return log(twitterError.error.red);
+      } else {
+        return log(('Tweet by @' + tweet.user.screen_name + ' favorited!').green);
       }
-      return log('Tweet by @' + tweet.user.screen_name + ' favorited!');
     });
   };
 
@@ -35,8 +39,8 @@
       track: settings.keywords.join(', ')
     });
     return stream.on('tweet', function(tweet) {
-      log('[' + moment().format('h:mm:ss a') + '] @' + tweet.user.screen_name + ': ' + tweet.text);
-      log('wating ' + settings.delay + 's to favorite...');
+      log(('[' + moment().format('h:mm:ss a') + ']').white + ' ' + ('@' + tweet.user.screen_name + ': ').cyan.bold + tweet.text.yellow);
+      log(('waiting ' + settings.delay + 's to favorite...').grey);
       return delay(settings.delay * 1000, function() {
         return favoriteTweet(tweet);
       });
--- src/favbot.coffee
+++ src/favbot.coffee
@@ -1,27 +1,33 @@
 twitter = require 'twit'
 moment = require 'moment'
+colors = require 'colors'
 settings = require './settings/settings.json'
 
 tweeper = new twitter settings.twitter
 
 log = (str) ->
-    console.log("[favbot] " + str)
+    console.log('[' + 'favbot'.green + ']', str)
 
 delay = (ms, func) ->
     setTimeout func, ms
 
 favoriteTweet = (tweet) ->
-    tweeper.post 'favorites/create', { id: "" + tweet.id_str }, (errors, tweet) ->
-        if errors
-            log errors
-            return
-        log 'Tweet by @' + tweet.user.screen_name + ' favorited!'
+    tweeper.post 'favorites/create', { id: tweet.id }, (error, tweet) ->
+        if error
+            twitterError = JSON.parse error.twitterReply
+            log (twitterError.error).red
+        else
+            log ('Tweet by @' + tweet.user.screen_name + ' favorited!').green
 
 startTracking = ->
     stream = tweeper.stream 'statuses/filter', { track: settings.keywords.join ', ' }
+
     stream.on 'tweet', (tweet) ->
-        log '[' + moment().format('h:mm:ss a') + '] @' + tweet.user.screen_name + ': ' + tweet.text
-        log 'wating ' + settings.delay + 's to favorite...'
+
+        log ('[' + moment().format('h:mm:ss a') + ']').white + ' ' + ('@' + tweet.user.screen_name + ': ').cyan.bold + (tweet.text).yellow
+
+        log ('waiting ' + settings.delay + 's to favorite...').grey
+
         delay settings.delay * 1000, -> favoriteTweet tweet
 
 startTracking()
